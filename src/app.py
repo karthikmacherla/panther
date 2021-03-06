82d1c1e58cb1d824f3b1907186fc9c4c4c4f0d0b
Karthik Macherla <kmacher@seas.upenn.edu>
2021-03-05 22:22:35 -0500

    Partially rearranged directories

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from celery_app import crawl, test
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://mongodb:27017/crawler"
+mongo = PyMongo(app)
 
 
 @app.route('/')
